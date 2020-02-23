7718c1670b1b12c3e96094c94283e6aedf0e279a
Pablo Maribondo <pablomgvieira@gmail.com>
2020-02-23 16:29:21 -0300

    Componente StartGameScreen implementado

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 
+import StartGameScreen from './screens/StartGameScreen';
 import Header from './components/Header';
 
 export default function App() {
   return (
     <View style={styles.screen}>
       <Header title='Guess a Number' />
+      <StartGameScreen />
     </View>
   );
 }
