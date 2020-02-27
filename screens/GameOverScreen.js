8c4bddf9815f1a4b2b9ce4346b9688ded1ef4d32
Pablo Maribondo <pablomgvieira@gmail.com>
2020-02-27 14:27:32 -0300

    Container de imagem na tela de Game Over

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, StyleSheet, Button, Image } from 'react-native';
 
 import BodyText from '../components/BodyText';
 import TitleText from '../components/TitleText';
@@ -8,6 +8,14 @@ const GameOverScreen = props => {
   return (
     <View style={styles.screen}>
       <TitleText>The Game is Over!</TitleText>
+      <View style={styles.imageContainer}>
+        <Image 
+          source={require('../assets/success.png')}
+          // source={{uri: 'https://image.shutterstock.com/z/stock-vector-mountain-climbing-route-to-peak-business-journey-path-in-progress-to-success-vector-concept-1054502702.jpg'}}
+          style={styles.image}
+          resizeMode='cover' 
+        />
+      </View>
       <BodyText>Number of Rounds: {props.roundsNumber}</BodyText>
       <BodyText>Number was: {props.userNumber}</BodyText>
       <Button title='NEW GAME' onPress={props.onRestart}/>
@@ -20,6 +28,19 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  imageContainer: {
+    width: 300,
+    height: 300,
+    borderRadius: 150,
+    borderWidth: 3,
+    borderColor: '#000',
+    overflow: 'hidden',
+    marginVertical: 30
+  },
+  image: {
+    width: '100%',
+    height: '100%'
   }
 });
 
